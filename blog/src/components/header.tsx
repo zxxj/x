2a34e28dbeb7d3d6da9051dad5d37c801270b078
zhangxinxin <1285367184@qq.com>
2025-08-26 00:38:53 +0800

    feat(header&router): router(添加首页重定向),header(抽离option逻辑)

--- blog/src/components/header.tsx
+++ blog/src/components/header.tsx
@@ -1,34 +1,73 @@
 import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Segmented } from 'antd';
-import { HomeIcon, ClipboardDocumentListIcon, UserIcon } from '@heroicons/react/24/solid'
-import { HomeIcon as HomeOutlineIcon, ClipboardDocumentListIcon as OutlineClipboardDocumentListIcon, UserIcon as OutlineUserIcon } from '@heroicons/react/24/outline'
+import {
+  HomeIcon,
+  ClipboardDocumentListIcon,
+  UserIcon,
+} from '@heroicons/react/24/solid';
+import {
+  HomeIcon as HomeOutlineIcon,
+  ClipboardDocumentListIcon as OutlineClipboardDocumentListIcon,
+  UserIcon as OutlineUserIcon,
+} from '@heroicons/react/24/outline';
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
-  const location = useLocation();
-  const [value, setValue] = useState(location.pathname);
-  const iconWidth = 20
-  const iconHeight = 20
+  const [value, setValue] = useState('/home');
+  const iconWidth = 20;
+  const iconHeight = 20;
+
+  const options = [
+    {
+      value: '/home',
+      label: '首页',
+      icon:
+        value === '/home' ? (
+          <HomeIcon width={iconWidth} height={iconHeight} />
+        ) : (
+          <HomeOutlineIcon width={iconWidth} height={iconHeight} />
+        ),
+    },
+    {
+      value: '/article',
+      label: '文章',
+      icon:
+        value === '/article' ? (
+          <ClipboardDocumentListIcon width={iconWidth} height={iconHeight} />
+        ) : (
+          <OutlineClipboardDocumentListIcon
+            width={iconWidth}
+            height={iconHeight}
+          />
+        ),
+    },
+    {
+      value: '/about',
+      label: '关于',
+      icon:
+        value === '/about' ? (
+          <UserIcon width={iconWidth} height={iconHeight} />
+        ) : (
+          <OutlineUserIcon width={iconWidth} height={iconHeight} />
+        ),
+    },
+  ];
 
   const handleClick = async (path: string) => {
-    setValue(path)
+    setValue(path);
     await navigate(path);
   };
 
   return (
     <div className="absolute w-full h-[40px] inset-0 text-white mt-5 flex justify-between items-center px-7 lg:px-20">
-      <img rel="icon" src="/logo2.svg" className='w-10' />
+      <img rel="icon" src="/logo2.svg" className="w-10" />
       <div>
         <Segmented
           size="middle"
           shape="round"
           value={value}
-          options={[
-            { value: '/home', label: '首页', icon: value === '/home' ? <HomeIcon width={iconWidth} height={iconHeight} /> : <HomeOutlineIcon width={iconWidth} height={iconHeight} /> },
-            { value: '/article', label: '文章', icon: value === '/article' ? <ClipboardDocumentListIcon width={iconWidth} height={iconHeight} /> : <OutlineClipboardDocumentListIcon width={iconWidth} height={iconHeight} /> },
-            { value: '/about', label: '关于', icon: value === '/about' ? <UserIcon width={iconWidth} height={iconHeight} /> : <OutlineUserIcon width={iconWidth} height={iconHeight} /> },
-          ]}
+          options={options}
           onChange={(val) => handleClick(val as string)}
         />
       </div>
--- blog/src/router/routes.tsx
+++ blog/src/router/routes.tsx
@@ -1,8 +1,10 @@
 import About from '@/views/about';
 import Article from '@/views/article';
 import Home from '@/views/home/index';
+import { Navigate } from 'react-router-dom';
 
 export const routes = [
+  { path: '/', element: <Navigate to="/home" replace /> },
   { path: '/home', name: '首页', element: <Home /> },
   { path: '/article', name: '文稿', element: <Article /> },
   { path: '/about', name: '关于', element: <About /> },
