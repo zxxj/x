8dd90ed813ff61ff17cd86f071e41c1631b03dcd
zhangxinxin <1285367184@qq.com>
2025-08-26 01:18:40 +0800

    feat(article): 定义文章数据模型,生成文章模块crud

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule],
+  imports: [PrismaModule, AuthModule, UserModule, ArticleModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
