fa92fa8125364f55ae498d37d856d9e8128a7eb0
zhangxinxin <1285367184@qq.com>
2025-08-27 10:14:57 +0800

    chore: 将使用Next构建前端与管理端,并且集成到单个项目 / 移除blog与admin这两个分离项目.

--- blog/src/views/about/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const About: React.FC = () => {
-  return <div className="text-white">about</div>;
-};
-
-export default About;
--- blog/src/views/article/index.tsx
+++
@@ -1,5 +0,0 @@
-const Article: React.FC = () => {
-  return <div className="text-white">文章</div>;
-};
-
-export default Article;
